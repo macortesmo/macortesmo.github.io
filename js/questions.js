e80bf81d155f1fb24432730a08a28073fcd958d7
Miguel Alejandro Cortes Molina <67292257+macortesmo@users.noreply.github.com>
2023-02-14 18:14:44 -0500

    Preguntas frecuentes al 100% y FAQ con JS

---
+++ js/questions.js
@@ -0,0 +1,24 @@
+(function(){
+    const titleQestions = [...document.querySelectorAll('.questions__title')];
+    console.log(titleQestions)
+    
+    titleQestions.forEach(question =>{
+        question.addEventListener('click', () =>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
+
+            addPadding.classList.toggle('questions__padding--add')
+            question.children[0].classList.toggle('questions__arrow--rotate');
+            
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`
+
+        })
+        
+    });
+
+})();
